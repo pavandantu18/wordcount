939f8a9d11f18ca482be4fad63f9f921e293e993
Pavan Kumar Reddy <67476441+pavandantu18@users.noreply.github.com>
2025-04-15 18:05:58 -0400

    Update word_count.py

--- word_count.py
+++ word_count.py
@@ -22,4 +22,5 @@ counts = text_file.flatMap(lambda line: line.split()) \
                   .map(lambda word: (word, 1)) \
                   .reduceByKey(lambda a, b: a + b)
 counts.saveAsTextFile(S3_OUTPUT)
+
 spark.stop()
