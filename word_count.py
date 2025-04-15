0c01bc66e47cf8e610f0b037f1f403909e5c1f79
Pavan Kumar Reddy <67476441+pavandantu18@users.noreply.github.com>
2025-04-15 17:59:41 -0400

    Update word_count.py

--- word_count.py
+++ word_count.py
@@ -5,8 +5,8 @@ AWS_ACCESS_KEY_ID = 'AKIAQQABDEF7X3T3MMHC'
 AWS_SECRET_ACCESS_KEY = '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'
 
 # S3 paths
-S3_INPUT = 's3a://s3://pavandbucket/input_file.txt'
-S3_OUTPUT = 's3a://s3://pavandbucket/output_folder/'
+S3_INPUT = 's3a://pavandbucket/input.txt'
+S3_OUTPUT = 's3a://pavandbucket/output_folder/'
 
 # Spark Session
 spark = SparkSession.builder \
